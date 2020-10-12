9eb24129d1cdbe7a05f5a626546a286e3feeeda0
xhj <hejun.xie@zju.edu.cn>
2020-10-12 17:29:26 +0800

    WRFDA engine implemented

--- pyCRO/core/_wrfda_scatter.py
+++ pyCRO/core/_wrfda_scatter.py
@@ -7,8 +7,63 @@ Used for comparision with radar operator
 Author: Hejun Xie
 Date: 2020-10-12 10:46:54
 LastEditors: Hejun Xie
-LastEditTime: 2020-10-12 11:12:39
+LastEditTime: 2020-10-12 17:22:17
 '''
 
+# Global imports
+import numpy as np
+np.warnings.filterwarnings('ignore')
+
+# Local imports
+from ..config.cfg import CONFIG
+from ..interpolation import Radial
+from ..constants import global_constants as constants
+
 def get_radar_observables_wrfda(list_subradials):
-    pass
+    """
+    Computes radar variables for all subradials, using WRFDA method, see (Dowell et al. 2011)
+    Args:
+        list_subradials: list of subradials (Radial claass instances) as
+            returned by the interpolation.py code, only simulate Horizontal Reflextivity (ZH)
+    
+    Returns:
+        A radial class instance containing the integrated radar observables
+    """
+    
+    # Get dimensions of subradials
+    num_beams = len(list_subradials) # Number of subradials (quad. pts)
+    idx_0 = int(num_beams/2) # Index of central subradial
+
+    central_radial = list_subradials[idx_0]
+
+    '''
+        Q<hydro> [kg/kg] * RHO [kg/m3] = Q<hydro>_v [kg/m3]
+    '''
+
+    qr = central_radial.values['QR_v']
+    qs = central_radial.values['QS_v']
+    qg = central_radial.values['QG_v']
+
+    zr = 3.63 * 1e9 * qr**1.75
+    zs = 9.80 * 1e8 * qs**1.75
+    zg = 4.33 * 1e10 * qg**1.75
+
+    ZH = zr + zs + zg
+
+    # Create outputs
+    rad_obs = {}
+    rad_obs['ZH'] = ZH
+
+    # Finally get vectors of distances, height and lat/lon at the central beam
+    heights_radar = central_radial.heights_profile
+    distances_radar = central_radial.dist_profile
+    lats = central_radial.lats_profile
+    lons = central_radial.lons_profile
+    mask = central_radial.mask
+
+    # Create final radial
+    radar_radial = Radial(rad_obs, mask, lats, lons, distances_radar,
+                          heights_radar)
+
+    return radar_radial
+
---
+++ pyCRO/test/wrfda_test.py
@@ -0,0 +1,126 @@
+'''
+Description: test for scatter
+Author: Hejun Xie
+Date: 2020-08-22 12:36:55
+LastEditors: Hejun Xie
+LastEditTime: 2020-10-12 17:17:32
+'''
+
+# unit test import
+import sys
+sys.path.append('/home/xhj/wkspcs/Radar-Operator/pyCRO/')
+
+# Global imports
+import numpy as np
+import pickle
+# import pyart
+
+# Local imports
+import pyCRO
+import pyart
+
+LOAD_MODEL = False
+LOAD_RADAR = False
+DEG = r'$^\circ$'
+
+cmap = {'ZH':'pyart_Carbone11', 'RVEL': 'pyart_BuOr8', 'ZDR': 'pyart_Carbone17',
+'KDP': 'pyart_EWilson17', 'PHIDP': 'pyart_Carbone42', 'RHOHV': 'pyart_GrMg16'}
+
+if __name__ == "__main__":
+    FILENAME = '../../../cosmo_pol/pathos/WRF/wsm6_test/wrfout_d01_2019-05-17_00_00_00'
+    a = pyCRO.RadarOperator(options_file='./option_files/wrfda_test.yml')
+    a.load_model_file(FILENAME, itime=40, load_pickle=LOAD_MODEL, pickle_file='mdl.pkl')
+
+    # print(a.dic_vars['T'])
+
+    if not LOAD_RADAR:
+        r = a.get_PPI(elevations = 1.494, plot_engine='pycwr')
+        with open("./ppi.pkl", "wb") as f:
+            pickle.dump(r, f)
+    else:
+        with open("./ppi.pkl", "rb") as f:
+            r = pickle.load(f)
+    
+    a.close()
+
+    # test PycwrRadop
+    # PRD = PycwrRadop('ppi',r)
+    PRD = r
+
+    print(PRD.scan_info)
+
+    import matplotlib.pyplot as plt
+    import cartopy.crs as ccrs
+    from pycwr.draw.RadarPlot import Graph, GraphMap
+
+    import matplotlib as mpl
+    mpl.use('Agg')
+
+    fields = ['dBZ']
+    PYCRO_name = {'dBZ': 'ZH',
+            'ZDR':'ZDR',
+            'KDP':'KDP',
+            'PhiDP':'PHIDP',
+            'CC': 'RHOHV',
+            'V': 'RVEL'
+            }
+
+    units = {'dBZ': 'dBZ',
+            'ZDR': 'dBZ',
+            'KDP': 'degrees/km',
+            'PhiDP': 'degrees',
+            'V': 'm/s',
+            'CC': '-'
+    }
+
+    isweep = 0
+    elevation = PRD.scan_info['fixed_angle'][isweep]
+
+    for field in fields:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection=ccrs.PlateCarree())
+        graph = GraphMap(PRD, ccrs.PlateCarree())
+        graph.plot_ppi_map(ax, isweep, field)
+        ax.set_title("Simulation {} [{}] \n e={:.3f} UTC: 2019-05-17 10:00".format(PYCRO_name[field],
+        units[field], float(elevation)), fontsize=15)
+        plt.savefig('simulation_{}_wrfda.png'.format(PYCRO_name[field]), dpi=300)
+
+        del fig, ax, graph
+
+    exit()
+    
+    # test PyartRadop
+    import matplotlib as mpl
+    mpl.use('Agg')
+    from pyart.graph import RadarMapDisplayBasemap
+    display = pyart.graph.RadarMapDisplayBasemap(r)
+    import matplotlib.pyplot as plt
+    plt.figure()
+
+    field = 'ZH'
+    vrange = (0, 60)
+    display.plot_ppi_map(field, 0, vmin=vrange[0], vmax=vrange[1],
+                     min_lon=113.5, max_lon=119, min_lat=37.5, max_lat=42.0,
+                     lon_lines=np.arange(38, 42, 1), projection='lcc',
+                     lat_lines=np.arange(113, 119, 1), resolution='h',
+                     lat_0=r.latitude['data'],
+                     lon_0=r.longitude['data'],
+                     cmap=cmap[field],
+                     title= 'Time: {}'.format(a.get_pos_and_time()['time']) + '\n' + \
+                            'Elevation: {}'.format(r.elevation['data'][0]) + DEG + '\n' + \
+                            r'$Z_{H}$')
+    # plot range rings at 50, 100, 200km
+    display.plot_range_ring(50., line_style='k-', lw=1.0)
+    display.plot_range_ring(100., line_style='k--', lw=1.0)
+    display.plot_range_ring(200., line_style='k-', lw=1.0)
+
+    # plots cross hairs
+    display.plot_line_xy(np.array([-300000.0, 300000.0]), np.array([0.0, 0.0]),
+                        line_style='k-', lw=1.2)
+    display.plot_line_xy(np.array([0.0, 0.0]), np.array([-300000.0, 300000.0]),
+                        line_style='k-', lw=1.2)
+
+    # display.plot_point(r.longitude['data'], r.latitude['data'])
+    
+    plt.savefig('ZH_ppi.png',dpi=300, bbox_inches='tight')
+    
