05ea9ee426b3b30049a2ccb791e913fb3465c707
xhj <hejun.xie@zju.edu.cn>
2020-11-03 22:57:23 +0800

    grapes and obs comparison finished

--- pyCRO/test/test_grapes_interface.py
+++ pyCRO/test/test_grapes_interface.py
@@ -3,12 +3,15 @@ Description: test GRAPES interface for radar operator
 Author: Hejun Xie
 Date: 2020-11-02 16:17:47
 LastEditors: Hejun Xie
-LastEditTime: 2020-11-03 16:05:42
+LastEditTime: 2020-11-03 22:48:53
 '''
 
 # unit test import
 import sys
 sys.path.append('/home/xhj/wkspcs/Radar-Operator/pyCRO/')
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 # Global imports
 import numpy as np
@@ -25,8 +28,18 @@ LOAD_MODEL = False
 LOAD_RADAR = False
 DEG = r'$^\circ$'
 
-cmap = {'ZH':'pyart_Carbone11', 'RVEL': 'pyart_BuOr8', 'ZDR': 'pyart_Carbone17',
-'KDP': 'pyart_EWilson17', 'PHIDP': 'pyart_Carbone42', 'RHOHV': 'pyart_GrMg16'}
+
+fields  = ['ZH', 'RVEL', 'ZDR']
+cmap = {'ZH':'pyart_Carbone11', 'RVEL': 'pyart_BuOr8', 'ZDR': 'pyart_Carbone17'}
+vrange  = {'ZH':  (0, 30),
+        'ZDR': (0, 1),
+        'RVEL': (-15, 15)}
+cmap    = {'ZH':  'pyart_Carbone11',
+        'ZDR': 'pyart_Carbone11',
+        'RVEL': 'pyart_BuOr8'}
+latex_name = {'ZH': r'$Z_{H}$',
+        'ZDR': r'$Z_{DR}$',
+        'RVEL': r'$V_r$'}
 
 if __name__ == "__main__":
 
@@ -38,7 +51,7 @@ if __name__ == "__main__":
     a.load_model_file(data_file_list, load_datetime=load_datetime, load_from_file=LOAD_MODEL, load_file='mdl.nc')
 
     if not LOAD_RADAR:
-        r = a.get_PPI(elevations = 1)
+        r = a.get_PPI(elevations = 0.5)
         # r = a.get_PPI_test(elevations = 1)
         with open("./ppi.pkl", "wb") as f:
             pickle.dump(r, f)
@@ -57,30 +70,28 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     plt.figure()
 
-    field = 'ZH'
-    vrange = (0, 25)
-    display.plot_ppi_map(field, 0, vmin=vrange[0], vmax=vrange[1],
-                     min_lon=113.5, max_lon=119, min_lat=37.5, max_lat=42.0,
-                     lon_lines=np.arange(38, 42, 1), projection='lcc',
-                     lat_lines=np.arange(113, 119, 1), resolution='h',
-                     lat_0=r.latitude['data'],
-                     lon_0=r.longitude['data'],
-                     cmap=cmap[field],
-                     title= 'Time: {}'.format(a.get_pos_and_time()['time']) + '\n' + \
-                            'Elevation: {}'.format(r.elevation['data'][0]) + DEG + '\n' + \
-                            r'$Z_{H}$')
-    # plot range rings at 50, 100, 200km
-    display.plot_range_ring(50., line_style='k-', lw=1.0)
-    display.plot_range_ring(100., line_style='k--', lw=1.0)
-    display.plot_range_ring(200., line_style='k-', lw=1.0)
+    for field in fields:
+        display.plot_ppi_map(field, 0, vmin=vrange[field][0], vmax=vrange[field][1],
+                            min_lon=114, max_lon=120, min_lat=38.0, max_lat=42.5,
+                            lon_lines=np.arange(38, 42, 1), projection='lcc',
+                            lat_lines=np.arange(113, 119, 1), resolution='h',
+                            lat_0=r.latitude['data'],
+                            lon_0=r.longitude['data'],
+                            cmap=cmap[field],
+                            title= 'Time: {}'.format(a.get_pos_and_time()['time']) + '\n' + \
+                                'Elevation: {:.1f}'.format(r.elevation['data'][0]) + DEG + '\n' + \
+                                latex_name[field])
+        # plot range rings at 50, 100, 200km
+        display.plot_range_ring(50., line_style='k-', lw=1.0)
+        display.plot_range_ring(100., line_style='k--', lw=1.0)
+        display.plot_range_ring(200., line_style='k-', lw=1.0)
 
-    # plots cross hairs
-    display.plot_line_xy(np.array([-300000.0, 300000.0]), np.array([0.0, 0.0]),
-                        line_style='k-', lw=1.2)
-    display.plot_line_xy(np.array([0.0, 0.0]), np.array([-300000.0, 300000.0]),
-                        line_style='k-', lw=1.2)
+        # plots cross hairs
+        display.plot_line_xy(np.array([-300000.0, 300000.0]), np.array([0.0, 0.0]),
+                            line_style='k-', lw=1.2)
+        display.plot_line_xy(np.array([0.0, 0.0]), np.array([-300000.0, 300000.0]),
+                            line_style='k-', lw=1.2)
 
-    # display.plot_point(r.longitude['data'], r.latitude['data'])
-    
-    plt.savefig('ZH_ppi_grapes.png',dpi=300, bbox_inches='tight')
+        plt.savefig('{}_ppi_grapes_{}.png'.format(load_datetime.strftime('%Y-%m-%d-%HUTC'), field), dpi=300, bbox_inches='tight')
 
+        plt.close()
---
+++ pyCRO/test/test_grapes_obs_plot.py
@@ -0,0 +1,80 @@
+'''
+Description: plot beijing 20191129 case
+Author: Hejun Xie
+Date: 2020-11-03 16:17:53
+LastEditors: Hejun Xie
+LastEditTime: 2020-11-03 22:54:26
+'''
+
+import pycwr
+import pyart
+import numpy as np
+import datetime as dt
+
+from pycwr.io import read_auto
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+# UTC 2019-11-29 12UTC
+current_time = dt.datetime(2019,11,29,18)
+file = r"/mnt/e/RADAR/19.11.29顺义X波段/29/BJXSY.20191129.180003.AR2.bz2"
+
+DEG = r'$^\circ$'
+
+PRD = read_auto(file)
+r = PRD.ToPyartRadar()
+
+print(PRD.scan_info)
+# print(PRD.fields[0])
+# print(r.fields.keys())
+# exit()
+
+import matplotlib as mpl
+mpl.use('Agg')
+from pyart.graph import RadarMapDisplayBasemap
+display = pyart.graph.RadarMapDisplayBasemap(r)
+import matplotlib.pyplot as plt
+plt.figure()
+
+fields  = ['reflectivity', 'differential_reflectivity', 'velocity']
+vrange  = {'reflectivity':  (0, 30),
+        'differential_reflectivity': (0, 1),
+        'velocity': (-15, 15)}
+cmap    = {'reflectivity':  'pyart_Carbone11',
+        'differential_reflectivity': 'pyart_Carbone11',
+        'velocity': 'pyart_BuOr8'}
+latex_name = {'reflectivity': r'$Z_{H}$',
+        'differential_reflectivity': r'$Z_{DR}$',
+        'velocity': r'$V_r$'}
+short_name = {'reflectivity':  'ZH',
+        'differential_reflectivity': 'ZDR',
+        'velocity': 'RVEL'}    
+
+sweep = 0
+
+for field in fields:
+    display.plot_ppi_map(field, sweep, vmin=vrange[field][0], vmax=vrange[field][1],
+                        min_lon=114, max_lon=120, min_lat=38.0, max_lat=42.5,
+                        lon_lines=np.arange(38, 42, 1), projection='lcc',
+                        lat_lines=np.arange(113, 119, 1), resolution='h',
+                        lat_0=r.latitude['data'],
+                        lon_0=r.longitude['data'],
+                        cmap=cmap[field],
+                        title= 'Time: {}'.format(str(current_time)) + '\n' + \
+                            'Elevation: {:.1f}'.format(r.elevation['data'][sweep]) + DEG + '\n' + \
+                            latex_name[field])
+    # plot range rings at 50, 100, 200km
+    display.plot_range_ring(50., line_style='k-', lw=1.0)
+    display.plot_range_ring(100., line_style='k--', lw=1.0)
+    display.plot_range_ring(200., line_style='k-', lw=1.0)
+
+    # plots cross hairs
+    display.plot_line_xy(np.array([-300000.0, 300000.0]), np.array([0.0, 0.0]),
+                        line_style='k-', lw=1.2)
+    display.plot_line_xy(np.array([0.0, 0.0]), np.array([-300000.0, 300000.0]),
+                        line_style='k-', lw=1.2)
+
+    plt.savefig('{}_ppi_obs_{}.png'.format(current_time.strftime('%Y-%m-%d-%HUTC'), short_name[field]), dpi=300, bbox_inches='tight')
+
+    plt.close()
