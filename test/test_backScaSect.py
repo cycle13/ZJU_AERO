37be872dc8732dcdf454a953bef2453f9fdf1edc
xhj <hejun.xie@zju.edu.cn>
2020-11-13 12:08:32 +0800

    move database tools into tool package

--- test/test_backScaSect.py
+++ test/test_backScaSect.py
@@ -4,18 +4,18 @@ of IITM, TM, and Rayleigh scattering approximation.
 Author: Hejun Xie
 Date: 2020-11-12 17:03:03
 LastEditors: Hejun Xie
-LastEditTime: 2020-11-13 09:51:15
+LastEditTime: 2020-11-13 11:55:26
 '''
 
 # unit test import
 import sys
+
 sys.path.append('/home/xhj/wkspcs/Radar-Operator/pyCRO/')
 
 # Global imports
 import numpy as np
 # np.set_printoptions(threshold=np.inf)
 import xarray as xr
-
 # Local imports
 from pyCRO.db import load_lut
 from pyCRO.hydrometeors.dielectric import dielectric_ice
@@ -40,9 +40,9 @@ def get_IITM_backScaSect(T, e):
     sz = lut.lookup_line(t=[T], e=[e]).squeeze()
 
     radar_xsect_h = 2*np.pi*(sz[:,0]-sz[:,1]-sz[:,2]+sz[:,3]) # [mm2]
-    print(radar_xsect_h)
+    radar_xsect_v = 2*np.pi*(sz[:,0]+sz[:,1]+sz[:,2]+sz[:,3]) # [mm2]
 
-    return radar_xsect_h
+    return radar_xsect_h, radar_xsect_v
     
 def get_TM_backScaSect(T, e):
     lut = load_lut('../pathos/lut/tmatrix_masc/lut_SZ_S_9_41_1mom.lut', engine='numpy')
@@ -57,7 +57,6 @@ def get_Rayleigh_backScaSect(T, f):
     Ki = Ki_squared(f, T)
     radar_xsect_h = np.pi**5 * D**6 / WAVELENGTH**4 * Ki * (RHO_S / RHO_I)**2 # [mm2]
 
-    print(radar_xsect_h)
     return radar_xsect_h
 
 if __name__ == '__main__':
@@ -68,16 +67,17 @@ if __name__ == '__main__':
     RHO_I = 916 # [kg m-3]
     RHO_S = 100 # [kg m-3]
     D = np.linspace(0.2, 20, 64) # [mm]
+    De = D * (RHO_S/RHO_I)**(1./3.) # [mm]
     C = 299792458. # [m s-1]
     WAVELENGTH = C / (f * 1.0E9) * 1000 # [mm]
 
-    bss_IITM = get_IITM_backScaSect(T, e)
+    bss_h_IITM, bss_v_IITM = get_IITM_backScaSect(T, e)
     # bss_TM = get_TM_backScaSect(T, e)
     bss_Rayleigh = get_Rayleigh_backScaSect(T, f)
 
     # start plot
-    import matplotlib.pyplot as plt
     import matplotlib as mpl
+    import matplotlib.pyplot as plt
     mpl.use('Agg')
 
     plt.rcParams['font.family'] = 'serif'
@@ -85,8 +85,10 @@ if __name__ == '__main__':
     
     ax.set_title('Horizontal Back scattering section')
 
-    ax.plot(D, bss_IITM, color='r', label='IITM orientation with preference')
-    ax.plot(D, bss_Rayleigh, color='k', label='Rayleigh scattering')
+    ax.plot(D, bss_h_IITM, color='r', ls='-', label='IITM orientation with preference Horizontal')
+    ax.plot(D, bss_v_IITM, color='r', ls='--', label='IITM orientation with preference Vertical')
+    ax.plot(D, bss_Rayleigh, color='k', ls='-', label='Rayleigh scattering Dmax')
+    ax.plot(De, bss_Rayleigh, color='k', ls='--', label='Rayleigh scattering De')
     
     ax.set_xlabel(r'Dmax [$mm$]', fontsize=12)
     ax.set_ylabel(r'Back scattering section [$mm^{2}$]', fontsize=12)
---
+++ tool/extract_lut.py
@@ -0,0 +1,150 @@
+'''
+Description: Extract Lookup Table from T-matrix output root directory and save it
+as a netCDF format file (LevelA-database)
+Author: Hejun Xie
+Date: 2020-09-17 10:04:49
+LastEditors: Hejun Xie
+LastEditTime: 2020-11-12 22:42:10
+'''
+
+import os
+import sys
+import glob
+import pandas as pd
+import numpy as np
+import xarray as xr
+from scipy.integrate import quad  
+
+RAW_DATA_ROOT = '/mnt/d/PYCRO/radardb_hexgon'
+LEVELA_DATA_TARGET = '/mnt/d/COSMO_POL/lut/iitm_masc/lut_SZ_S_9_41_1mom_LevelA.nc'
+NBETA = 91
+NELEVATION = 91
+ANAME = 'amplmatrix_fw_test.dat'
+PNAME = 'phasematrix_bw_test.dat'
+
+def get_amplmatrix(filename):
+
+    dataframe = pd.read_table(filename, delim_whitespace=True, skiprows=0, skipfooter=0, 
+    names=[ 'beta', 'elevation',
+            's11_r', 's11_i', 's12_r', 's12_i',
+            's21_r', 's21_i', 's22_r', 's22_i'])
+
+    dataframe['s11'] = dataframe['s11_r'] + dataframe['s11_i']*1j
+    dataframe['s12'] = dataframe['s12_r'] + dataframe['s12_i']*1j
+    dataframe['s21'] = dataframe['s21_r'] + dataframe['s21_i']*1j
+    dataframe['s22'] = dataframe['s22_r'] + dataframe['s22_i']*1j
+
+    return dataframe
+
+def get_phasematrix(filename):
+
+    dataframe = pd.read_table(filename, delim_whitespace=True, skiprows=0, skipfooter=0, 
+    names=['beta', 'elevation',   
+            'p11', 'p12', 'p13', 'p14',
+            'p21', 'p22', 'p23', 'p24',
+            'p31', 'p32', 'p33', 'p34',
+            'p41', 'p42', 'p43', 'p44'])
+
+    return dataframe
+
+def get_coords(directory):
+    subdirs = os.listdir(directory)
+    coords = []
+    for subdir in subdirs:
+        coords.append(float(subdir.split('_')[-1]))
+    coords.sort()
+    return coords
+
+def gaussian_pdf(std=10.0, mean=0.0):
+    """Gaussian PDF for orientation averaging.
+
+    Args:
+        std: The standard deviation in degrees of the Gaussian PDF
+        mean: The mean in degrees of the Gaussian PDF.  This should be a number
+          in the interval [0, 180)
+
+    Returns:
+        pdf(x), a function that returns the value of the spherical Jacobian- 
+        normalized Gaussian PDF with the given STD at x (degrees). It is 
+        normalized for the interval [0, 180].
+    """
+    norm_const = 1.0
+    def pdf(x):
+        return norm_const*np.exp(-0.5 * ((x-mean)/std)**2) * \
+            np.sin(np.pi/180.0 * x)
+    norm_dev = quad(pdf, 0.0, 180.0)[0]
+    # ensure that the integral over the distribution equals 1
+    norm_const /= norm_dev 
+    return pdf
+
+
+if __name__ == '__main__':
+
+    dims = ['aspect_ratio', 'temperature', 'Dmax', 'beta', 'elevation']
+    dir_asp = RAW_DATA_ROOT
+    dir_temp = os.path.join(dir_asp,os.listdir(dir_asp)[0])
+    dir_size = os.path.join(dir_temp,os.listdir(dir_temp)[0])
+
+    # add geometry size coordinate
+    C = 299792458.
+    # some band frequency for radar [GHZ]
+    Frequencies = {"S":2.7 ,"C":5.6 ,"X":9.41 ,"Ku":13.6 ,"Ka":35.6}
+    Lambda = C / Frequencies['X'] * 1e-9 * 1e+3 # [mm]
+    Dmax = np.array(get_coords(dir_size)) * Lambda / np.pi
+
+    coords = {'aspect_ratio': get_coords(dir_asp), 
+    'temperature': get_coords(dir_temp),
+    'Dmax': Dmax,
+    'beta': np.linspace(0, 90, NBETA),
+    'elevation': np.linspace(0, 90, NELEVATION)}
+
+    size = tuple([len(coords[dim]) for dim in dims])
+
+    real_variables      = [ 'p11_bw', 'p12_bw', 'p13_bw', 'p14_bw',
+                            'p21_bw', 'p22_bw', 'p23_bw', 'p24_bw',
+                            'p31_bw', 'p32_bw', 'p33_bw', 'p34_bw',
+                            'p41_bw', 'p42_bw', 'p43_bw', 'p44_bw']
+    complex_variables   = [ 's11_fw', 's12_fw', 's21_fw', 's22_fw']
+
+    datadic = {}
+    for real_variable in real_variables:
+        datadic[real_variable] = (dims, np.empty(size, dtype='float32'))
+    for complex_variable in complex_variables:
+        datadic[complex_variable] = (dims, np.empty(size, dtype='complex64'))
+
+    ds = xr.Dataset(datadic, coords)
+    
+
+    nodes = glob.glob('{}/asp_*/temp_*/size_*/'.format(RAW_DATA_ROOT))
+    for node in nodes:
+        asp = float(node.split('/')[-4].split('_')[-1])
+        temp = float(node.split('/')[-3].split('_')[-1])
+        size = float(node.split('/')[-2].split('_')[-1])
+        iasp = coords['aspect_ratio'].index(asp)
+        itemp = coords['temperature'].index(temp)
+        isize = get_coords(dir_size).index(size)
+        
+        file_phasematrix_bw = os.path.join(node, PNAME)
+        file_amplmatrix_fw = os.path.join(node, ANAME)
+        
+        phasematrix_bw = get_phasematrix(file_phasematrix_bw) # * (Lambda / (2*np.pi))**2 # [mm2]
+        amplmatrix_fw = get_amplmatrix(file_amplmatrix_fw) # * (Lambda / (2*np.pi)) # [mm]
+        
+        for real_variable in real_variables:
+            ds[real_variable][iasp, itemp, isize, ...] = \
+            phasematrix_bw[real_variable.split('_')[0]].to_numpy().reshape(NBETA, NELEVATION) * \
+                (Lambda / (2*np.pi))**2 # [mm2]
+
+        for complex_variable in complex_variables:
+            ds[complex_variable][iasp, itemp, isize, ...] = \
+            amplmatrix_fw[complex_variable.split('_')[0]].to_numpy().reshape(NBETA, NELEVATION) * \
+                (Lambda / (2*np.pi)) # [mm]    
+    
+    ds.coords["size_parameter"] = (("Dmax"), get_coords(dir_size))
+    
+    print(ds)
+
+    ds.to_netcdf(LEVELA_DATA_TARGET, engine="h5netcdf")
+
+    # with xr.open_dataset("./test.nc", engine="h5netcdf") as ds:
+    #     print(ds)
