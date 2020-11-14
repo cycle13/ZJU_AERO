c2c6cadefdcc0b9730504ff0eee365ef8c7a2438
xhj <hejun.xie@zju.edu.cn>
2020-11-14 12:37:08 +0800

    setup package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 @Author: Hejun Xie
 @Date: 2020-04-06 20:54:22
 LastEditors: Hejun Xie
-LastEditTime: 2020-11-06 22:36:46
+LastEditTime: 2020-11-14 12:36:48
 '''
 
 from setuptools import setup, Extension, Command
@@ -29,8 +29,8 @@ setup(  name        = "pyCRO",
         author='Hejun Xie - Zhejiang University',
         author_email='hejun.xie@zju.edu.cn',
         license='GPL-3.0',
-        packages=['pyCRO', 'pyCRO/interpolation', 'pyCRO/radar', 'pyCRO/utilities', 'pyCRO/constants',
-        'pyCRO/db', 'pyCRO/core', 'pyCRO/hydrometeors', 'pyCRO/beam', 'pyCRO/config', 'pyCRO/nwp', 
+        packages=['pyCRO', 'pyCRO/interp', 'pyCRO/radar', 'pyCRO/utils', 'pyCRO/const',
+        'pyCRO/db', 'pyCRO/core', 'pyCRO/hydro', 'pyCRO/beam', 'pyCRO/config', 'pyCRO/nwp', 
         'pyCRO/nwp/grapes', 'pyCRO/nwp/wrf'],
         package_data   = {'pyCRO/interpolation' : ['*.o','*.i','*.c','*.so']},
         install_requires=['numpy'],
