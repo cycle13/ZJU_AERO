2634be1b664e6c4ac366b0b1f0de8fb40a7f9e91
xhj <hejun.xie@zju.edu.cn>
2020-11-20 17:16:52 +0800

    rename package

--- ZJU_AERO/__init__.py
+++ ZJU_AERO/__init__.py
@@ -1,9 +1,9 @@
 '''
-@Description: __init__.py for pyCRO main package
+@Description: __init__.py for ZJU_AERO main package
 @Author: Hejun Xie
 @Date: 2020-04-06 20:57:03
 LastEditors: Hejun Xie
-LastEditTime: 2020-08-22 12:45:18
+LastEditTime: 2020-11-20 17:12:37
 '''
 
 from .radar_operator import RadarOperator
