f4971fb87755d1fe6ad10b85e26924ed3aa811d2
xhj <hejun.xie@zju.edu.cn>
2021-04-21 14:22:20 +0800

    rename and sort the folder

--- test/obs_graph/ground_X_190517_fangshan_wrf.py
+++ test/obs_graph/ground_X_190517_fangshan_wrf.py
@@ -3,14 +3,14 @@ Description: test pycwr
 Author: Hejun Xie
 Date: 2020-08-30 22:20:45
 LastEditors: Hejun Xie
-LastEditTime: 2020-11-11 10:12:57
+LastEditTime: 2021-04-21 14:02:38
 '''
 
 import pycwr
 
 from pycwr.io import read_auto
 
-file = r"../pathos/RADAR/19.5.17房山-2/BJXFS.20190517.100000.AR2.bz2"
+file = r"/mnt/e/RADAR/19.5.17房山-2/BJXFS.20190517.100000.AR2.bz2"
 
 PRD = read_auto(file)
 PyartRadar = PRD.ToPyartRadar()
--- test/test_convert_to_nc.py
+++ test/test_convert_to_nc.py
@@ -3,7 +3,7 @@ Description: test transfer from GRAPES modelvar to nc
 Author: Hejun Xie
 Date: 2020-10-28 16:33:40
 LastEditors: Hejun Xie
-LastEditTime: 2021-04-14 11:30:20
+LastEditTime: 2021-04-21 14:18:02
 '''
 
 # unit test import
@@ -13,9 +13,13 @@ sys.path.append('/home/xhj/wkspcs/Radar-Operator/ZJU_AERO/')
 from ZJU_AERO.nwp.grapes import convert_to_nc_general_ctl
 from ZJU_AERO.nwp.grapes import convert_to_nc_specific_ctl
 
-# CTLFILE = '/mnt/e/GRAPES/north_china_snowfall_20191112/grapes2019112900_10.ctl'
-# NC = '/mnt/e/GRAPES/north_china_snowfall_20191112/grapes2019112900_10.nc'
-# transf2nc_specific_ctl(CTLFILE, NC, var='all', v_grid_type='ml')
+# [A]. SPECIFIC
+# CTLFILE = '/mnt/f/modelvar/2020090500/model.ctl_202009050000900'
+# NC = '/mnt/f/modelvar/2020090500/modelvar202009050000900.nc'
+# convert_to_nc_specific_ctl(CTLFILE, NC, var=['zz', 'pi', 'th', 
+#     'u', 'v', 'w', 'Qv', 'Qc', 'Qr', 'Qi', 'Qs', 'Qg'], 
+#     geobox=[125, 135, 18, 32], v_grid_type='ml')
 
+# [B]. GENERAL
 CTLFILE = '../pathos/GRAPES/north_china_snowfall_20191112/grapes2019112900ctl'
 convert_to_nc_general_ctl(CTLFILE, var='all', v_grid_type='ml')
